51244820e82be7b4b82fbfe320f4028c51866d57
unknown <gusdudco6@naver.com>
2024-11-16 17:25:33 +0900

    the Java

---
+++ demo/src/test/java/com/example/demo/ArchClassesTests.java
@@ -0,0 +1,36 @@
+package com.example.demo;
+
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
+import com.tngtech.archunit.library.dependencies.SlicesRuleDefinition;
+
+
+@AnalyzeClasses(packagesOf = DemoApplication.class)
+public class ArchClassesTests {
+
+    @ArchTest
+    ArchRule controllerClassRule = ArchRuleDefinition.classes().that().haveSimpleNameEndingWith("Controller")
+            .should().accessClassesThat().haveSimpleNameEndingWith("Service")
+            .orShould().accessClassesThat().haveSimpleNameEndingWith("Repository");
+
+
+    @ArchTest
+    ArchRule domainPackageRule = ArchRuleDefinition.classes().that().resideInAPackage("..domain..")
+            .should().onlyBeAccessed().byClassesThat()
+            .resideInAnyPackage("..study..", "..member..", "..domain..");
+
+    @ArchTest
+    ArchRule memberPackageRule = ArchRuleDefinition.noClasses().that().resideInAPackage("..domain..")
+            .should().accessClassesThat().resideInAPackage("..member..");
+
+    @ArchTest
+    ArchRule studyPackageRule = ArchRuleDefinition.noClasses().that().resideOutsideOfPackage("..study..")
+            .should().accessClassesThat().resideInAnyPackage("..study..");
+
+    @ArchTest
+    ArchRule freeOfCycles = SlicesRuleDefinition.slices().matching("..inflearnthejavatest.(*)..")
+            .should().beFreeOfCycles();
+
+}
